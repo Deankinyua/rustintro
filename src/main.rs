34e2de37500d3e74e8af9b791a7fa24944d13319
Dean Kinyua <kinyuadean@gmail.com>
2024-12-23 12:05:57 +0300

    a crate is a single compilation unit in Rust

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,28 @@
 // * Most lines of Rust end in a semicolon
 // * cargo check, cargo run and cargo build
 
-use std::io;
+// use std::io;
+
+// fn main() {
+//     println!("Guess the number!");
+
+//     println!("Please input your guess.");
+
+//     let mut guess = String::new();
+
+//     io::stdin()
+//         .read_line(&mut guess)
+//         .expect("Failed to read line");
+
+//     println!("You guessed: {}", guess);
+// }
+
+// importing a module in a file named foo.rs 
+mod foo;
+
+// * a crate is a single compilation unit in Rust
+// * a crate can be compiled into a library or a binary 
 
 fn main() {
-    println!("Guess the number!");
-
-    println!("Please input your guess.");
-
-    let mut guess = String::new();
-
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-
-    println!("You guessed: {}", guess);
+    foo::hello();
 }
