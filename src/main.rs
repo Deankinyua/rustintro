e7579cba6aae27bb61ef453fc99f6f1ab642c984
Dean Kinyua <kinyuadean@gmail.com>
2024-12-23 06:04:35 +0300

    Rust ........more like c but also has thinks like cargo fmt that resemble mix format

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,20 @@
-fn main() {
-    // * The presence of the ! indicates a you are calling a macro 
+// * The presence of the ! indicates a you are calling a macro
 
-// * Most lines of Rust end in a semicolon 
-    println!("Hello, world!");
+// * Most lines of Rust end in a semicolon
+// * cargo check, cargo run and cargo build
+
+use std::io;
+
+fn main() {
+    println!("Guess the number!");
+
+    println!("Please input your guess.");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You guessed: {}", guess);
 }
