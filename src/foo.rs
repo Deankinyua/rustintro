b318db57a7f09867668e3a7c7c8eb30effa659d5
Dean Kinyua <kinyuadean@gmail.com>
2024-12-26 19:17:11 +0300

    dkkd

--- src/foo.rs
+++ src/foo.rs
@@ -1,3 +1,12 @@
 pub fn hello() {
-    println!("Hello, world!");
+    let number1: i64 = -445;
+
+    let y1: f64 = 3.09979797577483393930006099;
+    let y2: f32 = 3.09979797577483393930006099;
+    let truncated  = -5 / 3;
+
+    println!("the number is a signed integer of size 32 - {number1}");
+    println!("32 bit float has less precision - {y2}");
+    println!("than 64 bit float {y1}");
+    println!("Quotient is : {truncated}");
 }
