ef6312cd26bc4e50d22ea26413f128836a5cb95a
Dean Kinyua <kinyuadean@gmail.com>
2024-12-27 06:39:08 +0300

    because shadowing is creating a completely new variable, we can even change the variable's type, something that is simply impossible with mutable variables

--- src/foo.rs
+++ src/foo.rs
@@ -1,20 +1,38 @@
 // use rand::{thread_rng, Rng};
-
+// * Using constants in Rust is indeed helpful as
+// * it helps Us adhere to the DRY principle
+// * and in case something changes we only modify it at that particular place
+// * module attributes in Elixir play the same role except they are discarded after compilation
 
 pub fn variables() {
+    let mut y = 5;
+    println!("The value of y is: {y}");
+    y = 6;
+    println!("The value of y is: {y}");
+    // let secret_number = rand::thread_rng().gen_range(1..=100);
+    const FOUR_HOURS_IN_SECONDS: u32 = 60 * 60 * 4;
+    // * a Constant must always be assigned to a constant expression
+    // * this will fail because
+    // * constants are always immutable
+    // const FOUR_HOURS_IN_SECONDS: u32 = 60 * 60 * secret_number;
+    println!("Four hours in seconds is: {FOUR_HOURS_IN_SECONDS}");
+    let x = 5;
+    println!("The value of x here is: {x}");
 
-        let mut x = 5;
-        println!("The value of x is: {x}");
-        x = 6;
-        println!("The value of x is: {x}");
-        // let secret_number = rand::thread_rng().gen_range(1..=100);
-        const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 4;
+    let x = x + 1;
 
-        // * a Constant must always be assigned to a constant expression 
-        // * this will fail 
-        // * constants are always immutable
-        // const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * secret_number;
-        
-        println!("Four hours in seconds is: {THREE_HOURS_IN_SECONDS}");
+    {
+        let x = x * 2;
+        println!("The value of x in the inner scope is: {x}");
+    }
 
+    println!("The value of x is: {x}");
+
+    let spaces = "   ";
+    let _spaces = spaces.len();
+    println!(
+        "Shadowing is effectively creating a new 
+        variable with the same name as the 
+        previous one meaning the previous value will be discarded"
+    )
 }
