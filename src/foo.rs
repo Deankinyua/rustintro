bc5cc5462e78377367eb7c3bde7faf0461ea5c81
Dean Kinyua <kinyuadean@gmail.com>
2024-12-28 05:32:45 +0300

    When defining functions you must include the parameter type . The fn keyword is used to declare a function

--- src/foo.rs
+++ src/foo.rs
@@ -2,8 +2,20 @@ pub fn functions() {
     // ? Functions in Rust
 
     another_function();
+    value(5);
+    print_labeled_measurement(5, 'h');
 }
 
 fn another_function() {
     println!("Another function.");
+}
+
+// You must include the type of the parameter in the function definition
+fn value(x: i32) {
+    println!("The value of x is: {x}");
+}
+
+// With multiple parameters
+fn print_labeled_measurement(value: i32, unit_label: char) {
+    println!("The measurement is: {value}{unit_label}");
 }
