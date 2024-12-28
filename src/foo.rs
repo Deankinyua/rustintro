6ad5f1192b56ae71021ea4b99a0b597339167f7f
Dean Kinyua <kinyuadean@gmail.com>
2024-12-28 06:13:57 +0300

    Expressions return a value .....Statements do not ......Functions in Rust normally end in an expression because an expression returns something

--- src/foo.rs
+++ src/foo.rs
@@ -4,6 +4,7 @@ pub fn functions() {
     another_function();
     value(5);
     print_labeled_measurement(5, 'h');
+    block_expression();
 }
 
 fn another_function() {
@@ -18,4 +19,24 @@ fn value(x: i32) {
 // With multiple parameters
 fn print_labeled_measurement(value: i32, unit_label: char) {
     println!("The measurement is: {value}{unit_label}");
+}
+
+// Understanding statements and expressions in Rust
+
+// ? Statements are instructions that perform some action and do not return a value.
+// ? Expressions evaluate to a resultant value.
+
+// expressions return values e.g x + 2
+// statements do not !! e.g declaring a variable and assigning it to a value is a statement
+// fn statement_example() {
+// let x = (let y = 6);
+// }
+
+fn block_expression() {
+    let y = {
+        let x = 3;
+        x + 1
+    };
+
+    println!("The value of y is: {y}");
 }
