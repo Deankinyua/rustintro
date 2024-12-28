51a95d3fd06310ead72946e34675dff5b57722a4
Dean Kinyua <kinyuadean@gmail.com>
2024-12-28 05:20:57 +0300

    You can define your functions before or after calling them . ....Rust does not care .....as long as they are in the right scope

--- src/foo.rs
+++ src/foo.rs
@@ -1,62 +1,9 @@
-pub fn variables() {
-    let x = 2.5555555669484949; // f64
+pub fn functions() {
+    // ? Functions in Rust
 
-    let y: f64 = 3.000344; // f64
-    let _quotient = 56.7 / 32.2;
+    another_function();
+}
 
-    let _truncated = -5 / 3; // Results in -1
-
-    // remainder
-    let _remainder = 43 % 5;
-
-    let _t = true;
-    println!("The multiplication of {x} and {y} is {}", x * y);
-
-    // This is a tuple in Rust ....more like an array in JavaScript
-    // ? Tuples cannot grow or shrink in size and have a fixed length
-
-    let tup = (500, 6.4, 1);
-
-    // * Pattern Matching used for destructuring just like in Elixir
-    let (x, y, z) = tup;
-
-    println!("The value of x is: {x}");
-    println!("The value of y is: {y}");
-    println!("The value of z is: {z}");
-
-    let tuple2 = ("Kerware", 94, "Makueni Boys");
-
-    // Tuples can also be accessed by their index as seen below
-
-    println!("The value of the first element in Tuple 2 is {}", tuple2.0);
-    println!("The value of the second element in Tuple 2 is {}", tuple2.1);
-
-    // * Arrays in Rust must have a fixed length and the elements
-    // * inside an Array must be of the same type
-    // * if you want a similar data structure that grows and shrinks use a vector
-    // * Store data that is of a predefined length
-    let months = [
-        "January",
-        "February",
-        "March",
-        "April",
-        "May",
-        "June",
-        "July",
-        "August",
-        "September",
-        "October",
-        "November",
-        "December",
-    ];
-
-    println!("Array elements being accessed by the index {} ", months[11]);
-
-    let a = [1, 2, 3, 4, 5];
-
-    let first = a[0];
-    let second = a[1];
-    println!(
-        "The first and the second elements in the array are {first} and {second} respectively"
-    );
+fn another_function() {
+    println!("Another function.");
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 // * importing a module in a file named foo.rs
 mod foo;
-
+// The main function in Rust is very important
 fn main() {
-    foo::variables();
+    foo::functions();
 }
