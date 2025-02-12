45d4152a6c731aaeee204795332536f3156db31b
Dean Kinyua <kinyuadean@gmail.com>
2025-02-12 19:18:24 +0300

    enums give you a way of saying a value is one of a possible set of values

--- src/foo.rs
+++ src/foo.rs
@@ -1,12 +1,13 @@
+// * enums give you a way of saying a value is one of a possible set of values
+// * in this case an Ip address can be either version 4 or version 6 but not both
+
+enum IpAddr {
+    V4(String),
+    V6(String),
+}
+
 pub fn hello() {
-    let number1: i64 = -445;
+    let home = IpAddr::V4(String::from("127.0.0.1"));
 
-    let y1: f64 = 3.09979797577483393930006099;
-    let y2: f32 = 3.09979797577483393930006099;
-    let truncated  = -5 / 3;
-
-    println!("the number is a signed integer of size 32 - {number1}");
-    println!("32 bit float has less precision - {y2}");
-    println!("than 64 bit float {y1}");
-    println!("Quotient is : {truncated}");
+    let loopback = IpAddr::V6(String::from("::1"));
 }
